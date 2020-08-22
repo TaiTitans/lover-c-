019f05c419a1d9240ff0165f4a8676f410045261
Phan Phát Tài <70010376+TaiTitans@users.noreply.github.com>
2020-08-22 10:13:51 +0700

    Add files via upload

---
+++ lover.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+/*                                                   
+                                                  
+                                                  
+                                                  
+     (@@@@@@ @@ /@@@@@@  @@   @@% @@ &@@@@@.      
+        @@   @@    @@   &@@%  @@@ @@ &@           
+        @@   @@    @@   @@@@  @@@@@@ *@@@@@       
+        @@   @@    @@  ,@.*@, @@ @@@     *@.      
+        @@   @@    @@  @@@@@@ @@  @@ &@((%@.      
+                                                  
+                                                  
+*/
+using namespace std;
+
+int main (){
+	string co;
+	cout <<"Ban muon co ny khong ?"<<endl;
+	cin >> co;
+	cout << "Goodboiz nhu may ma doi co ny?";
+	return 0;
+}
